77bd354461df14274e22e93379e9234767d214d2
justin <justinchang1124@gmail.com>
2022-10-14 21:03:18 -0400

    correct open_dcm_folder, abs to use_abs

--- image_testing.py
+++ image_testing.py
@@ -12,14 +12,14 @@ import matplotlib.pyplot as plt
 # recursively gets the locations of all DCM files in abs_base_path/folder
 # abs_dir_path: an absolute path to a directory
 # abs: if True, return absolute locations (otherwise return relative locations)
-def dcm_dir_list(abs_dir_path, abs=False):
+def dcm_dir_list(abs_dir_path, use_abs=False):
     if not (os.path.isabs(abs_dir_path) and os.path.isdir(abs_dir_path)):
         raise ValueError("Not an absolute value path!")
     cur_cwd = os.getcwd()
     os.chdir(abs_dir_path)
     result = list(glob.iglob('**/*.dcm', recursive=True))
     os.chdir(cur_cwd)
-    if abs:
+    if use_abs:
         n = len(result)
         for i in range(n):
             result[i] = os.path.join(abs_dir_path, result[i])
@@ -49,13 +49,7 @@ def resize_dcm_image(dcm_image, shape):
 
 # opens all DCM images in a folder
 def open_dcm_folder(abs_dir_path):
-    dcm_filenames = dcm_dir_list(abs_dir_path)
-    n_dcm_files = len(dcm_filenames)
-    result = [None] * n_dcm_files
-    for i in range(n_dcm_files):
-        abs_dcm_file = os.path.join(abs_dir_path, dcm_filenames[i])
-        result[i] = open_dcm_image(abs_dcm_file)
-    return result
+    return open_dcm_images(dcm_dir_list(abs_dir_path, abs=True))
 
 
 # animates a list of DCM images [each a 2D numpy array]
@@ -92,4 +86,5 @@ def animate_dcm_images(dcm_images):
     # 17 milliseconds per frame yields 58-60 FPS
     fig.canvas.mpl_connect('button_press_event', on_click)
     anim = FuncAnimation(plt.gcf(), update, frames=n, interval=17, blit=True)
-    return anim
+    plt.show()
+    input("To continue, close any open plots and press enter!")
