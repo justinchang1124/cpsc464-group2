1d3a1859f350d8e4d174946922f1ce09a3d0c07e
justin <justinchang1124@gmail.com>
2022-11-26 17:17:33 -0500

    improved downsample, filtering UI

--- downsample.py
+++ downsample.py
@@ -5,22 +5,38 @@ import os
 abs_proj_path = 'C:/Users/justin/PycharmProjects/cpsc464-group2'
 data_path = 'image_data/manifest-1654812109500/Duke-Breast-Cancer-MRI'
 abs_data_path = os.path.join(abs_proj_path, data_path)
-abs_dcm_files = dcm_utils.dcm_dir_list(abs_data_path, use_abs=True)
+abs_dcm_files = dcm_utils.dcm_dir_list(abs_data_path, ret_abs=True)
+n_dcm_files = len(abs_dcm_files)
 
-i_shape = (128, 128)
+min_x = 1000000000  # 1 billion
+min_y = 1000000000  # 1 billion
 
-input("Press enter to proceed with downsampling ...")
+for i in range(n_dcm_files):
+    dcm_data = dcm_utils.open_dcm_with_image(abs_dcm_files[i])
+    dcm_shape = dcm_data.pixel_array.shape
+    min_x = min(min_x, dcm_shape[0])
+    min_y = min(min_y, dcm_shape[1])
+    if i % 100 == 0:
+        print("{}/{}: ({}, {})".format(i, n_dcm_files, min_x, min_y))
 
-# attempt downsampling
-for abs_dcm_file in abs_dcm_files[0:10]:
-    dcm_utils.downsample_dcm_file(abs_dcm_file, i_shape)
+print("Final resolution: {}, {}".format(min_x, min_y))
+input("Type any non-empty string and press enter to proceed with downscaling ...")
 
-# can we still open it?
-for abs_dcm_file in abs_dcm_files[0:10]:
+i_shape = (min_x, min_y)  # intended shape
+i_shape = (512, 512)
+
+# test: attempting to downscale and reopen
+test_n = 10
+
+for abs_dcm_file in abs_dcm_files[:test_n]:
+    dcm_utils.downscale_dcm(abs_dcm_file, i_shape)
+
+for abs_dcm_file in abs_dcm_files[:test_n]:
     test_image = dcm_utils.open_dcm_image(abs_dcm_file)
     print(test_image.shape)
     print(np.mean(test_image))
     print(np.std(test_image))
 
+# proceed with full downscaling
 for abs_dcm_file in abs_dcm_files:
-    dcm_utils.downsample_dcm_file(abs_dcm_file, i_shape)
+    dcm_utils.downscale_dcm(abs_dcm_file, i_shape)
--- filtering.py
+++ filtering.py
@@ -3,6 +3,26 @@ import os
 import re
 import numpy as np
 
+
+def id_of_file_slice(abs_dcm_file):
+    truncate_left = re.search("[0-9]+[.]dcm$", abs_dcm_file)
+    truncate_right = re.search("[0-9]+", truncate_left.group(0))
+    return int(truncate_right.group(0))
+
+
+def filter_file_slice(abs_dcm_file, slice_min, slice_max):
+    dcm_utils.validate_le(slice_min, slice_max)
+    slice_num = id_of_file_slice(abs_dcm_file)
+    if slice_num > slice_max or slice_num < slice_min:
+        os.unlink(abs_dcm_file)
+        print("Unlinked {}".format(abs_dcm_file))
+
+
+def id_of_patient_dir(patient_dir):
+    id_as_match = re.search("[0-9]+$", patient_dir)
+    return int(id_as_match.group(0))
+
+
 abs_proj_path = 'C:/Users/justin/PycharmProjects/cpsc464-group2'
 data_path = 'image_data/manifest-1654812109500/Duke-Breast-Cancer-MRI'
 abs_data_path = os.path.join(abs_proj_path, data_path)
@@ -13,32 +33,14 @@ dcm_files_data = []
 for file in dcm_files:
     dcm_files_data.append(file.split('\\'))
 
-
-def id_of_file_slice(abs_dcm_file):
-    truncate_left = re.search("[0-9]+[.]dcm$", abs_dcm_file)
-    truncate_right = re.search("[0-9]+", truncate_left.group(0))
-    return int(truncate_right.group(0))
-
-
-def filter_file_slice(abs_dcm_file, min_slice, max_slice):
-    if min_slice > max_slice:
-        raise ValueError("Cannot permit min_slice > max_slice!")
-    slice_num = id_of_file_slice(abs_dcm_file)
-    if slice_num > max_slice or slice_num < min_slice:
-        os.unlink(abs_dcm_file)
-
-
-def id_of_patient_dir(patient_dir):
-    id_as_match = re.search("[0-9]+$", patient_dir)
-    return int(id_as_match.group(0))
-
-
 start_slices_path = 'resources/start_slices.txt'
 end_slices_path = 'resources/end_slices.txt'
 abs_start_slices_path = '{}/{}'.format(abs_proj_path, start_slices_path)
 abs_end_slices_path = '{}/{}'.format(abs_proj_path, end_slices_path)
 start_slices = np.genfromtxt(abs_start_slices_path, dtype=int)
 end_slices = np.genfromtxt(abs_end_slices_path, dtype=int)
+
+input("Type any non-empty string and press enter to proceed with slice filtering ...")
 
 for i in range(n_dcm_files):
     patient_id = id_of_patient_dir(dcm_files_data[i][0])
